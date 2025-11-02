09a65fd6a8042c6e1e55a973f050399d2ce127bb
Kinyua Christabell <kinyuachristabell@gmail.com>
2025-11-02 22:44:49 +0300

    Add Render URL to ALLOWED_HOSTS

--- myproject/settings.py
+++ myproject/settings.py
@@ -24,9 +24,11 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-d@43hw*7s(kkgz=vio7eqz#j&$8(uc9j%qyh$y5!elsi&d-j9d'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['ecommerce-app-2-7xxf.onrender.com']
+
+
 
 
 # Application definition
